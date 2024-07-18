9966a9bcd923dfb1184b5e0cc01acab5758c911a
dreamanlan <dreamanlan@tencent.com>
2024-07-18 14:35:31 +0800

    Update AdbForward.cs

--- Assets/Editor/AdbForward.cs
+++ Assets/Editor/AdbForward.cs
@@ -7,7 +7,15 @@ public class AdbForward {
     public static void DoCommand_34999()
     {
 #if UNITY_EDITOR_WIN
-        System.Diagnostics.Process.Start("adb.exe", "forward tcp:34999 localabstract:Unity-" + Application.identifier);
+        try {
+            var p = System.Diagnostics.Process.Start("adb.exe", "forward tcp:34999 localabstract:Unity-" + Application.identifier);
+            if (null == p) {
+                EditorUtility.DisplayDialog("adb", "./adb forward tcp:34999 localabstract:Unity-" + Application.identifier, "ok");
+            }
+        }
+        catch {
+            EditorUtility.DisplayDialog("adb", "./adb forward tcp:34999 localabstract:Unity-" + Application.identifier, "ok");
+        }
 #else
         EditorUtility.DisplayDialog("adb", "./adb forward tcp:34999 localabstract:Unity-" + Application.identifier, "ok");
 #endif
@@ -17,7 +25,15 @@ public class AdbForward {
     public static void DoCommand_12000()
     {
 #if UNITY_EDITOR_WIN
-        System.Diagnostics.Process.Start("adb.exe", "forward tcp:12000 tcp:12000");
+        try {
+            var p = System.Diagnostics.Process.Start("adb.exe", "forward tcp:12000 tcp:12000");
+            if (null == p) {
+                EditorUtility.DisplayDialog("adb", "./adb forward tcp:12000 tcp:12000", "ok");
+            }
+        }
+        catch {
+            EditorUtility.DisplayDialog("adb", "./adb forward tcp:12000 tcp:12000", "ok");
+        }
 #else
         EditorUtility.DisplayDialog("adb", "./adb forward tcp:12000 tcp:12000", "ok");
 #endif
